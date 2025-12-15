9d1fe0d7958c7613c6b4d8084004ec324032762d
Iustin Desrobitu <141618792+elle7777@users.noreply.github.com>
2025-12-15 00:03:16 +0000

    Add files via upload

---
+++ Week 1/week1_ex2.js
@@ -0,0 +1,15 @@
+// Week 1 Lab Ex. 2
+// @Iustin Desrobitu 2526313
+
+let num1 = 2; // setting the first variable
+let num2 = 5; // setting the second variable
+
+let addSum = num1 + num2; // setting the sum for the addition operation 
+let subSum = num1 - num2; // setting the sum for the subtraction opeation 
+let mulSum = num1 * num2; // setting the sum for the multiplication operation 
+let divSum = num1 / num2; // setting the sum for the division operation 
+
+console.log('add: | ' + num1 + ' + ' + num2 + ' = ' + addSum)
+console.log('sub: | ' + num1 + ' - ' + num2 + ' = ' + subSum)
+console.log('mul: | ' + num1 + ' * ' + num2 + ' = ' + mulSum)
+console.log('sub: | ' + num1 + ' / ' + num2 + ' = ' + divSum)
